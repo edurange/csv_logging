81c3b0c4aac5c03f34237101d11db8e9cd467532
Alexander King <32370879+alexanderjvking@users.noreply.github.com>
2019-04-25 11:24:42 -0700

    Update start_ttylog.sh

--- start_ttylog.sh
+++ start_ttylog.sh
@@ -5,33 +5,33 @@
      TTY_CMD=$(tty)
      TTY=${TTY_CMD:5}
      #HN=$(hostname)
-     HOST=$(hostname)
+     #HOST=$(hostname)
      #EXP=$(echo $HN | awk -F. '{print $(NF - 1)}')
      #PROJ=$(echo $HN | awk -F. '{print $(NF)}')
      USER=$(whoami)
 
      sudo mkdir -p /var/log/ttylog/
 
-     if [ -e "/var/log/ttylog/count.$HOST" ]; then
-         CNT=$(cat /var/log/ttylog/count.$HOST)
+     if [ -e "/var/log/ttylog/count.$(hostname)" ]; then
+         CNT=$(cat /var/log/ttylog/count.$(hostname))
          let CNT++
-         echo $CNT > /var/log/ttylog/count.$HOST
+         echo $CNT > /var/log/ttylog/count.$(hostname)
      else
-         sudo touch /var/log/ttylog/count.$HOST
-         sudo chmod ugo+rw /var/log/ttylog/count.$HOST
-         echo "0" > /var/log/ttylog/count.$HOST
-         CNT=$(cat /var/log/ttylog/count.$HOST)
+         sudo touch /var/log/ttylog/count.$(hostname)
+         sudo chmod ugo+rw /var/log/ttylog/count.$(hostname)
+         echo "0" > /var/log/ttylog/count.$(hostname)
+         CNT=$(cat /var/log/ttylog/count.$(hostname))
      fi
 
      export TTY_SID=$CNT
-     LOGPATH=/var/log/ttylog/ttylog.$HOST.$CNT
+     LOGPATH=/var/log/ttylog/ttylog.$(hostname).$CNT
 
      sudo touch $LOGPATH
      sudo chmod ugo+rw $LOGPATH
 
      echo "starting session w tty_sid:$CNT" >> $LOGPATH
-     echo "User prompt is ${USER}@${HOST}" >> $LOGPATH
-     echo "Home directory is ${HOME}" >> $LOGPATH
+     echo "User prompt is ${$(whoami)}@${$(hostname)}" >> $LOGPATH
+     echo "Home directory is ${$(hostname)}" >> $LOGPATH
 
      sudo /usr/local/src/ttylog $TTY >> $LOGPATH 2>/dev/null &
 
