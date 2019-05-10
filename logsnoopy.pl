df8fa75b7288438d7a9c91dfb5f1637bf9630ae0
Alexander King <32370879+alexanderjvking@users.noreply.github.com>
2019-05-09 17:32:12 -0700

    Update logsnoopy.pl

--- logsnoopy.pl
+++ logsnoopy.pl
@@ -21,9 +21,7 @@
 
 while(1)
 {
-    system("cat /var/log/ttylog/ttylog.* > /var/log/ttylog/alltty.\$\(hostname\)");
-    system("python3 /usr/local/src/analyze.py /var/log/ttylog/alltty.\$\(hostname\)" . " /var/log/ttylog/cli.\$\(hostname\).csv");
-    #system("/usr/local/src/getting_started_milestones.sh")
-    #system("cp /var/log/ttylog/cli." . $host . ".csv /proj/" . $proj . "/exp/" . $exp . "/logs/cli." . $host . ".csv");
+    system("cat /var/log/ttylog/ttylog.* > /var/log/ttylog/alltty.\$\(hostname\).\$\(whoami\)");
+    system("python3 /usr/local/src/analyze.py /var/log/ttylog/alltty.\$\(hostname\).\$\(whoami\)" . " /var/log/ttylog/cli.\$\(hostname\).\$\(whoami\).csv");
     sleep(60);
 }
