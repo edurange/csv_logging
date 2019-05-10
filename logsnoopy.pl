04218fac8a2c19d3be29bca722847d708c84f0ce
Alexander King <32370879+alexanderjvking@users.noreply.github.com>
2019-05-09 18:59:21 -0700

    Update logsnoopy.pl

--- logsnoopy.pl
+++ logsnoopy.pl
@@ -21,7 +21,7 @@
 
 while(1)
 {
-    system("cat /var/log/ttylog/ttylog.* > /var/log/ttylog/alltty.\$\(hostname\).\$\(whoami\)");
-    system("python3 /usr/local/src/analyze.py /var/log/ttylog/alltty.\$\(hostname\).\$\(whoami\)" . " /var/log/ttylog/cli.\$\(hostname\).\$\(whoami\).csv");
+    system("cat /usr/local/src/logs/ttylog.* > /usr/local/src/logs/alltty.\$\(hostname\).\$\(whoami\)");
+    system("python3 /usr/local/src/analyze.py /usr/local/src/logs/alltty.\$\(hostname\).\$\(whoami\)" . " /usr/local/src/logs/cli.\$\(hostname\).\$\(whoami\).csv");
     sleep(60);
 }
